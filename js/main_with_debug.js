b939a15de1f98c57c624f6f4afcfc0f88c339d45
RachelRen-RSH <71853197+RachelRen-RSH@users.noreply.github.com>
2022-02-09 10:29:46 -0600

    comment

--- js/main_with_debug.js
+++ js/main_with_debug.js
@@ -1,4 +1,4 @@
-
+// define a array that contains the city population and names
 var cityPop = [
     {
         city: 'Madison',
@@ -17,7 +17,7 @@ var cityPop = [
         population: 27244
     }
 ];
-///////////////////////////// create a table ////////////////////////
+// create a table structure for the html and function use
 var body = document.getElementsByTagName("body")[0];
 var tbl = document.createElement("table");
 for (var j = 0; j < 5; j++) {
@@ -27,21 +27,24 @@ for (var j = 0; j < 5; j++) {
     console.log("eadxqerxq");
 }
 body.appendChild(tbl);
-/////////////////////////////////////////////////////////////////////
 
+// the function to add the content of the table created before
 function addColumns(cityPop) {
     console.log("wdef");
 
+    // loop for each row to add the content of each cell, when i equals to 0, it means it is the first row and the title of each column
     document.querySelectorAll("tr").forEach(function (row, i) {
         console.log("zheli");
         if (i == 0) {
             row.insertAdjacentHTML('beforeend', '<th>City</th>');
             row.insertAdjacentHTML('beforeend', '<th>Population</th>');
             row.insertAdjacentHTML('beforeend', '<th>City Size</th>');
-        } else {
+        }
+        // if it is not the first row, use the array created before to add the content for each cell
+        else {
             console.log("wheircnhwicnrhjwinctjhir");
             var citySize;
-
+            // define the level of the size of city based on the population
             if (cityPop[i - 1].population < 100000) {
                 citySize = 'Small';
 
@@ -59,12 +62,13 @@ function addColumns(cityPop) {
     });
 };
 
+// the function to change the color and add the effects for the web page
 function addEvents() {
-
+    // declare a function to change the style of the table so that when the mouse touch the table, it changes the style
     document.querySelector("table").addEventListener("mouseover", function () {
         console.log("qqqqqqqqqqqqqqq");
         var color = "rgb(";
-
+        // define the color and use for loop to generate a random color
         for (var i = 0; i < 3; i++) {
 
             var random = Math.round(Math.random() * 255);
@@ -77,16 +81,18 @@ function addEvents() {
             } else {
                 color += ")";
             };
-
+            // add the random color to the table
             document.querySelector("table").style.color = color;
         }
     })
-
+    // declare a function to show an alert when clicking the table
     function clickme() {
 
         alert('Hey, you clicked me!');
     };
     document.querySelector("table").addEventListener("click", clickme)
 };
+
+// call the two functions to show in the screen and perform 
 addEvents();
 window.onload = addColumns(cityPop);
